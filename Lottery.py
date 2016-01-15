8dfbeb2d5a296e5fe81cd1de2c34f2d121b3c27c
Jay Williams <course@jaywilliams.me>
2016-01-15 11:36:12 +0000

    Neatened up

--- Lottery.py
+++ Lottery.py
@@ -7,7 +7,7 @@ none = int(0)
 import random
 import time
 #This is all the defines of to find out what the numbers where
-def numbers():
+def numbers(number1):
     if number1 == answer1:
         print("You have got",number1,"correct")
         global ammount
@@ -17,7 +17,7 @@ def numbers():
         global none
         none += 1
         
-def numbers2():
+def numbers2(number2):
     if number2 == answer2:
         print("You have got ",number2,"correct")
         global ammount
@@ -28,7 +28,7 @@ def numbers2():
         global none
         none += 1
         
-def numbers3():
+def numbers3(number3):
     if number3 == answer3:
         print("You have got ",number3,"correct")
         global ammount
@@ -38,7 +38,7 @@ def numbers3():
         global none
         none += 1
         
-def numbers4():
+def numbers4(number4):
     if number4 == answer4:
         print("You have got ",number4,"correct")
         global ammount
@@ -48,7 +48,7 @@ def numbers4():
         global none
         none += 1
         
-def numbers5():
+def numbers5(number5):
     if number5 == answer5:
         print("You have got",number5,"correct")
         global ammount
@@ -59,7 +59,7 @@ def numbers5():
         none += 1
         
         
-def numbers6():
+def numbers6(number6):
     if number6 == answer6:
         print("You have got number",number6,"correct")
         global ammount
@@ -69,7 +69,7 @@ def numbers6():
         global none
         none += 1
         
-def numbers7():
+def numbers7(number7):
     if number7 == answer7:
         print("You have got number",number7,"correct")
         global ammount
@@ -83,29 +83,22 @@ def numbers7():
 #Welcomes to the program    
 print("Welcome to the lottery")
 #Lets the user pick the numbers
-number1 = input("Pick your first number:")
-number2 = input("Pick your second number:")
-number3 = input("Pick your third number:")
-number4 = input("Pick your Fourth number:")
-number5 = input("Pick your fith number")
-number6 = input("Pick your six number")
-number7 = input("Pick your seventh number")
 #Picks randoms numbers
-answer1 = random.random()
-answer2 = random.random()
-answer3 = random.random()
-answer4 = random.random()
-answer5 = random.random()
-answer6 = random.random()
-answer7 = random.random()
+answer1 = random.randint(0,59)
+answer2 = random.randint(0,59)
+answer3 = random.randint(0,59)
+answer4 = random.randint(0,59)
+answer5 = random.randint(0,59)
+answer6 = random.randint(0,59)
+answer7 = random.randint(0,59)
 #Calls the defines
-numbers()
-numbers2()
-numbers3()
-numbers4()
-numbers5()
-numbers6()
-numbers7()
+numbers(input("Pick your first number:"))
+numbers2(input("Pick your second number:"))
+numbers3(input("Pick your third number:"))
+numbers4(input("Pick your Fourth number:"))
+numbers5(input("Pick your fith number"))
+numbers6(input("Pick your six number"))
+numbers7(input("Pick your seventh number"))
 #Wait's for half a second
 time.sleep(0.5)
 #Displayes the amount number the person got right
